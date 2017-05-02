dd4ec9a4903959f4f1beff8ff1a7d070740fe64d
Shannon Moeller <me@shannonmoeller.com>
2017-05-02 11:05:57 -0400

    Updted install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 
 set -ex
+
+# zsh
+
+ln -s "$(dirname "$(realpath "$0")")/.zshrc" "$HOME/.zshrc"
+source "$HOME/.zshrc"
+
+# homebrew
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -13,6 +20,8 @@ brew install the_silver_searcher
 brew install tmux
 brew install tree
 
+# node
+
 n lts
 
 npm install -g diff-so-fancy
@@ -21,6 +30,6 @@ npm install -g editorconfig
 npm install -g http-server
 npm install -g stylelint_d
 
-ln -s "$(dirname "$(realpath "$0")")/.zshrc" "$HOME/.zshrc"
+# vim
 
 vim +PlugInstall +qa
