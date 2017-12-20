cff5c9dd5664a2e49a55e9b5b49338753ca95cfb
Shannon Moeller <me@shannonmoeller.com>
2017-12-20 10:48:18 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,3 +37,8 @@ source "$HOME/.zshrc"
 # vim
 
 vim +PlugInstall +qa
+
+# bin
+curl --create-dirs -o ~/.config/up/up.sh https://raw.githubusercontent.com/shannonmoeller/up/master/up.sh
+curl https://raw.githubusercontent.com/shannonmoeller/bulk/master/bulk.sh > ~/bin/bulk
+chmod +x ~/bin/bulk
