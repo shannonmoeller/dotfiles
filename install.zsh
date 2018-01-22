b7a34c3f66dd61c35f4d6df077154334987127d7
Shannon Moeller <me@shannonmoeller.com>
2018-01-22 14:58:30 -0700

    make install script safer

--- install.zsh
+++ install.zsh
@@ -6,10 +6,11 @@ for file in $(ls -d src/.*); do
 	ln -s "$PWD/$file" "$HOME/$(basename "$file")"
 done
 
-# bin
+# manual
 
 curl --create-dirs -o "$HOME/.config/bulk/bulk.sh" https://raw.githubusercontent.com/shannonmoeller/bulk/master/bulk.sh
 curl --create-dirs -o "$HOME/.config/up/up.sh" https://raw.githubusercontent.com/shannonmoeller/up/master/up.sh
+curl --create-dirs -o "$HOME/.vim/autoload/plug.vim" https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # homebrew
 
@@ -30,11 +31,15 @@ brew install zsh-syntax-highlighting
 
 # node
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-[ -f "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh"
+if ! [[ -x "$(command -v nvm 2>/dev/null)" ]]; then
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+fi
 
-nvm install lts/*
-nvm alias default lts/*
+if ! [[ -x "$(command -v npm 2>/dev/null)" ]]; then
+    [ -f "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh"
+    nvm install lts/*
+    nvm alias default lts/*
+fi
 
 npm install -g diff-so-fancy
 npm install -g editorconfig
@@ -48,7 +53,5 @@ npm install -g stylelint
 npm install -g vmd
 
 # vim
-
-curl --create-dirs -o ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim +PlugInstall +qa
