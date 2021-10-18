0022159d7866c27b1914e65d056373e03578cfe3
Shannon Moeller <me@shannonmoeller.com>
2021-10-18 09:50:45 -0600

    npm globals

--- setup/npm.sh
+++ setup/npm.sh
@@ -5,11 +5,16 @@ if ! [ -x "$(command -v fnm 2>/dev/null)" ]; then
     /usr/bin/env bash -c "$(curl -fsSL https://fnm.vercel.app/install)" -- --skip-shell
 fi
 
-fnm install 16
-fnm use 16
+VERSION="${1:-16}"
+
+fnm install "$VERSION"
+fnm use "$VERSION"
 npm install -g diff-so-fancy
 npm install -g eslint
+npm install -g livery
 npm install -g npm-check-updates
+npm install -g npmrc
 npm install -g prettier
 npm install -g stylelint
 npm install -g stylelint-config-recommended
+npm instlal -g vercel
