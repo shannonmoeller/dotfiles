8869e28ff7b04df7805a68c0de74dde2b8b81062
Shannon Moeller <me@shannonmoeller.com>
2021-11-23 18:54:33 +0000

    add _d to npm.sh

--- setup/npm.sh
+++ setup/npm.sh
@@ -14,10 +14,12 @@ fi
 
 npm install -g diff-so-fancy
 npm install -g eslint
+npm install -g eslint_d
 npm install -g livery
 npm install -g npm-check-updates
 npm install -g npmrc
 npm install -g prettier
+npm install -g prettier_d_slim
 npm install -g stylelint
 npm install -g stylelint-config-recommended
 npm install -g vercel
